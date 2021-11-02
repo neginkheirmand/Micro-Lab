a14ccf04a50d3b8371a386058a85e37771ae2a5d
neginkheirmand <neginkheirmand@gmail.com>
2021-11-02 17:39:50 +0330

    done with the potentiomotor code

--- Lab4_ServoMotor/sketch_potentiomotor/sketch_potentiomotor.ino
+++ Lab4_ServoMotor/sketch_potentiomotor/sketch_potentiomotor.ino
@@ -1,9 +1,25 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h> 
 
+// Declare the Servo pin
+int servoPin = 3;
+
+int analogPin = 3;
+// Create a servo object
+Servo servo;
+
+String input = "0";
+int incomingByte = 0; // for incoming serial data
+
+void setup() {
+  Serial.begin(9600);
+  // We need to attach the servo to the used pin number
+  servo.attach(servoPin);
+  delay(1000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  long value = analogRead(analogPin);
+  value = map(value, 0,1023,0,180);
+  servo.write(value);
+  Serial.println(value);
 }
