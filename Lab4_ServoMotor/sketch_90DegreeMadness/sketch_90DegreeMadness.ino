4d27707f9a50b330d5ce9d7c7d8f523e28d60f6f
neginkheirmand <neginkheirmand@gmail.com>
2021-11-02 17:39:32 +0330

    updated code for first part of the 4th lab

--- Lab4_ServoMotor/sketch_90DegreeMadness/sketch_90DegreeMadness.ino
+++ Lab4_ServoMotor/sketch_90DegreeMadness/sketch_90DegreeMadness.ino
@@ -4,21 +4,21 @@
 // Declare the Servo pin
 int servoPin = 3;
 // Create a servo object
-Servo Servo1;
+Servo servo;
 
 void setup() {
   Serial.begin(9600);
   // We need to attach the servo to the used pin number
-  Servo1.attach(servoPin);
+  servo.attach(servoPin);
 }
 
 void loop() {
   // Make servo go to 0 degrees
-  Servo1.write(0);
+  servo.write(0);
   Serial.println("0");
   delay(1000);
   // Make servo go to 90 degrees
-  Servo1.write(90);
+  servo.write(90);
   Serial.println("90");
   delay(1000);
 
