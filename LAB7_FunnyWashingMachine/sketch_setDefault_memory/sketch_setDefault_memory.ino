102e37a339fc8fe0e8c74725269b7a37284aeb37
neginkheirmand <neginkheirmand@gmail.com>
2021-12-12 17:16:35 +0330

    a simple sketch that will set the default values needed for the machine to work properly in the eeprom

---
+++ LAB7_FunnyWashingMachine/sketch_setDefault_memory/sketch_setDefault_memory.ino
@@ -0,0 +1,61 @@
+#include <Wire.h>
+
+#define ADDR_Ax 0b000 //A2, A1, A0
+#define ADDR (0b1010 << 3) + ADDR_Ax //creating the address of the slave eeprom (the signals that define which slave and in what type of communication R/W)
+
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  //the default
+  writeI2CByte(0, 4);
+  for (byte i = 1; i <=4 ; i++) {
+    writeI2CByte(i, i);
+  }
+  //the custom (by default also has the value of 1-2-3-4)
+  writeI2CByte(6, 4);
+  for (byte i = 1; i <=4 ; i++) {
+    writeI2CByte(i+6, i);
+  }
+  
+  for (byte i = 0; i <= 20; i++) {
+    byte b = readI2CByte(i);
+    String a = "";
+    a = a + String(i);
+    a = a + "  ";
+    a = a + String(b);
+    Serial.println(a);
+  }
+  
+}
+
+void loop() {
+   
+}
+
+void writeI2CByte(byte data_addr, byte data){
+  Wire.beginTransmission(ADDR);
+  delay(10);
+  Wire.write(data_addr);
+  delay(10);
+  Wire.write(data);
+  delay(10);
+  Wire.endTransmission();
+}
+
+byte readI2CByte(byte data_addr){
+  byte data = NULL;
+  Wire.beginTransmission(ADDR);
+  delay(10);
+  Wire.write(data_addr);
+  delay(10);
+  Wire.endTransmission();
+  delay(10);
+  Wire.requestFrom(ADDR, 1); //retrieve 1 returned byte
+  delay(10);
+  if(Wire.available()){
+    data = Wire.read();
+    delay(10);
+  }
+  return data;
+}
